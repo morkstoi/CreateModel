fe939efd8f1b59bf32a92bacf70f09d139e957cc
theac <theac@DESKTOP-LEARA2L>
2024-01-08 21:30:00 +0300

    Add project files.

---
+++ CreateModel/CreateModelPlugin.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateModel
+{
+
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CreateModelPlugin : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+
+
+
+
+            Transaction transaction = new Transaction(doc, "Построение стены");
+            transaction.Start();
+            List<Wall> newWalls = WallBuilder.CreateWallsFromRectangle(doc, "Уровень 1", "Уровень 2", 10000, 5000);
+
+
+            transaction.Commit();
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
